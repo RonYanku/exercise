a5d3f824970a5e064e3f4d9ee6576f0511fa6054
Ron Yanku <ron.yanku86@gmail.com>
2021-10-10 14:25:25 +0300

    whatever

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,17 @@ import { AppComponent } from './app.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { PresentFirstNameComponent } from './components/present-first-name/present-first-name.component';
 import { ToastModule } from 'ng-uikit-pro-standard';
 import { PaperlessExerciseComponent } from './paperless-exercise/paperless-exercise.component';
-import { AppRoutingModule } from './app-routing.module'
+import { AppRoutingModule } from './app-routing.module';
+import { PresentReverseFirstNameComponent } from './paperless-exercise/components/present-reverse-first-name/present-reverse-first-name.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PresentFirstNameComponent,
-    PaperlessExerciseComponent
+    PaperlessExerciseComponent,
+    PresentReverseFirstNameComponent
   ],
   imports: [
     BrowserModule,
